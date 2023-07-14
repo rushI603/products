b9e84dcca817a2ce62e114960ed33e7f824d6fd9
rushI603 <lollarushyendra@gmail.com>
2023-07-14 10:00:42 +0530

    Filter feature implemented

--- components/Products.jsx
+++ components/Products.jsx
@@ -10,9 +10,10 @@ import redlike from '../assets/liked.svg'
 import filterImage from '../assets/filter.svg' 
 
 const Products = () => {
-    const filter = ["clothing","jewellers","men","women","electronics"]
+    const filter = ["clothing","jewelery","men","women","electronics"]
     const [products, setProducts] = useState([])
-    const [selectedFilter, setSelectedFilter] = useState([])
+    const [selectedFilter, setSelectedFilter] = useState(["jewelery"])
+    // const [filterObj, setFilterObj] = useState({"clothing":0,"jewelery":0,"men":0,"women":0,"electronics":0})
     const [liked,setLiked] = useState({})
     const load = async ()=> {
         const url = "https://fakestoreapi.com/products";
@@ -23,13 +24,20 @@ const Products = () => {
             }
         )
     }
+
+    function check (category) {
+        console.log("check called")
+        for (let i = 0; i < selectedFilter.length; i++) {
+            if(category.includes(selectedFilter[i]))return true;
+        }
+        return false;
+    }
     useEffect(()=>{
         load(); 
 
     },[])
     useEffect(()=>{
         products.forEach(({id})=>{
-            console.log(id)
             setLiked(prev=> { return {...prev, [id]:id%3?0:1}})
         })
     },[products])
@@ -38,8 +46,10 @@ const Products = () => {
             <div className="flex justify-center flex-col items-center">
                 <div className="flex justify-around pt-4 w-full">
                     <div className="flex gap-x-2 ">
-                        {filter.map((item)=> <div className='bg-[#ffffff] opacity-50  text-[#97bf0d] px-2 rounded-full p-1'>
-                            {item}</div>
+                        {filter.map((item)=> 
+                            <div className='bg-[#ffffff] opacity-50  text-[#97bf0d] px-2 rounded-full p-1' onClick={()=>setSelectedFilter((prev)=>[...prev,item])}>
+                                {item}
+                            </div>
                             )}
                     </div>
                     <div></div>
@@ -54,8 +64,8 @@ const Products = () => {
                     </div>
                 </div>
                 <div className='grid grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-y-[15px]'>
-                    {products && !selectedFilter.length &&
-                        products.map(({id, image,title,price,rating})=>(
+                    {products && selectedFilter.length==0 &&
+                        products.map(({id, image,title,price,rating, category})=>(
                             <div className='relative rounded-lg w-[300px] flex flex-col justify-center items-center'>
                                 <div className='absolute top-4 left-4' onClick={()=>setLiked({...liked,[id]:!liked[[id]]})}>
                                     <Image
@@ -89,10 +99,42 @@ const Products = () => {
                         ))
                     }
                     {
-                        products && selectedFilter.length &&
-                        <div>
-                            
-                        </div>
+                        products && selectedFilter.length!=0 &&
+                        products.map(({id, image,title,price,rating, category})=>(
+                            check(category) ?
+                            <div className='relative rounded-lg w-[300px] flex flex-col justify-center items-center'>
+                                <div className='absolute top-4 left-4' onClick={()=>setLiked({...liked,[id]:!liked[[id]]})}>
+                                    <Image
+                                    className='rounded-full'
+                                    width={15}
+                                    height={15}
+                                    src={liked[id]?redlike:like}
+                                    alt='like'
+                                    />
+                                </div>
+                                <div className="image pt-4 w-[200px] h-[200px]">
+                                    <Image
+                                        className='bg-[#ffffff] mark border-2 w-full h-full rounded-lg aspect-[4/5]  object-contain'
+                                        width={150}
+                                        height={150}
+                                        src={image}   
+                                        alt='product image'                     
+                                    />
+                                </div>
+                                <div className="flex gap-4 pt-[20px] px-[50px]">
+                                    <div className='font-bold text-[15px]'>{title.slice(0,26)}...</div>
+                                    <div className='font-bold text-[15px]'><sup>$</sup>{Math.floor(price)}{price%1==0?<sup>.00</sup>:<sup >{(price+"").substring((price+"").indexOf('.'))}</sup>}</div>
+                                </div>
+                                
+                                <div className="flex gap-4 py-[10px] px-[50px] flex-col relative items-start w-full  ">
+                                    <Rating className='rating absolute left-[-20px] scale-75' data-cell={rating.count} name="read-only"  value={rating.rate} />
+                                    <a href="#" className='text-[#97bf0d] border-2 rounded-full p-1 text-sm'>Add to cart</a>
+
+                                </div>
+                            </div>
+                            :
+                            <div className='hidden'></div>
+                        ))
                     }
                     
                 </div>
